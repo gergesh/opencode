deb92b996874f4e989528fe032df30365c319fb7
Yoav Shai <gergesh@gmail.com>
2025-12-21 01:44:16 +0200

    Properly handle SIGTERM and SIGINT
    Fixes issue where killing the server when deployed doesn't work too well

--- packages/opencode/src/cli/cmd/serve.ts
+++ packages/opencode/src/cli/cmd/serve.ts
@@ -25,7 +25,16 @@ export const ServeCommand = cmd({
       hostname,
     })
     console.log(`opencode server listening on http://${server.hostname}:${server.port}`)
+
+    const stop = async () => {
+      console.log("stopping server...")
+      await server.stop()
+      process.exit(0)
+    }
+
+    process.on("SIGTERM", stop)
+    process.on("SIGINT", stop)
+
     await new Promise(() => {})
-    await server.stop()
   },
 })
